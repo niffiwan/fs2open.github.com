69060a6c76b645a789514f444b6b2875bb84c556
niffiwan <niffiwan.scp@gmail.com>
2015-09-15 21:49:54 +1000

    bah! fix formatting error

--- code/graphics/generic.cpp
+++ code/graphics/generic.cpp
@@ -337,8 +337,8 @@ void generic_render_eff_stream(generic_anim *ga)
 			bm_unload(ga->eff.next_frame, 0, true);
 			if (ga->current_frame == ga->num_frames-1)
 			{
-					snprintf(frame_name, MAX_FILENAME_LEN, "%s_0001", ga->filename);
-					bm_reload(ga->eff.next_frame, frame_name);
+				snprintf(frame_name, MAX_FILENAME_LEN, "%s_0001", ga->filename);
+				bm_reload(ga->eff.next_frame, frame_name);
 			}
 		}
 	#ifdef TIMER
