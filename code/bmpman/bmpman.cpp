438b72460a571e63a5d3e288a991b7d5c3b813b0
niffiwan <niffiwan.scp@gmail.com>
2016-02-15 20:47:26 +1000

    Add total_time output param to bm_load_animation
    Also convert int to bool, and replace NULL with nullptr

--- code/bmpman/bmpman.cpp
+++ code/bmpman/bmpman.cpp
@@ -1182,7 +1182,7 @@ bool bm_load_and_parse_eff(const char *filename, int dir_type, int *nframes, int
 	return true;
 }
 
-int bm_load_animation(const char *real_filename, int *nframes, int *fps, int *keyframe, int can_drop_frames, int dir_type) {
+int bm_load_animation(const char *real_filename, int *nframes, int *fps, int *keyframe, float *total_time, bool can_drop_frames, int dir_type) {
 	int	i, n;
 	anim	the_anim;
 	CFILE	*img_cfp = NULL;
@@ -1343,7 +1343,7 @@ int bm_load_animation(const char *real_filename, int *nframes, int *fps, int *ke
 		return -1;
 	}
 
-	if ((can_drop_frames) && (type == BM_TYPE_ANI)) {
+	if ((can_drop_frames == true) && (type == BM_TYPE_ANI)) {
 		if (Bm_low_mem == 1) {
 			reduced = 1;
 			anim_frames = (anim_frames + 1) / 2;
@@ -1486,7 +1486,7 @@ int bm_load_either(const char *filename, int *nframes, int *fps, int *keyframe, 
 		*nframes = 0;
 	if (fps != NULL)
 		*fps = 0;
-	int tidx = bm_load_animation(filename, nframes, fps, keyframe, can_drop_frames, dir_type);
+	int tidx = bm_load_animation(filename, nframes, fps, keyframe, nullptr, can_drop_frames, dir_type);
 	if (tidx == -1) {
 		tidx = bm_load(filename);
 		if (tidx != -1 && nframes != NULL)
